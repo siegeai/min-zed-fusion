8da006ab4c333d48f909e1d537014526c03b6e79
Eric Wang <eric.zizhou.w@gmail.com>
2025-08-23 22:40:33 -0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,16 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+// main.tsx
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+const repo = "min-zed-fusion"; // exact repo name
+
+createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <BrowserRouter basename={`/${repo}`}>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
