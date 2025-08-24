668171c33d98df5e2db75cea39a08f717c6eecc2
Eric Wang <eric.zizhou.w@gmail.com>
2025-08-23 22:45:29 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: `/min-zed-fusion/`,
+  base: `/`,
   server: { host: "::", port: 8080 },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: { alias: { "@": path.resolve(__dirname, "./src") } },
